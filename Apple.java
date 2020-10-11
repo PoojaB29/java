e10d995e946f2cedc65d290c7cb121f9da7eff71
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-11 20:57:22 +0530

    Add files via upload
    The program in Apple.java shows how to implement vector.

---
+++ Apple.java
@@ -0,0 +1,29 @@
+//Created by Pooja
+//On 5 oct 2019
+//The program shows the implementation of vector(dynamic array).
+
+import java.lang.*;
+import java.io.*; 
+import java.util.*;
+class A {
+public static void main(String args[]) {
+Vector v = new Vector(12,1);
+v.add(0,1);
+v.add(1,2);
+v.add(2,3);
+v.add(3,4);
+v.add(4,5);
+v.addElement(new String("Hi"));
+v.addElement(new String("World"));
+v.addElement(new String("Home"));
+v.addElement(new Double(8.8));
+v.addElement(new Double(4.4));
+v.add(10,"A");
+v.add(11,"B");
+boolean a=v.contains("Hi");
+v.removeElementAt(4);
+System.out.println("Display first and last element in vector:"+elementAt(0)+" "+lastElement());
+System.out.println("List of Elements is:");
+System.out.println(v);
+}
+}
