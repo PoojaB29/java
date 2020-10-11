40ebc6c020265fe13366f07085fdd2d7320e45ef
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-11 20:49:43 +0530

    Add files via upload
    The program A.java explains inheritance using the extends keyword.

---
+++ A.java
@@ -0,0 +1,56 @@
+//Created by Pooja 
+//5 oct 2019
+//program file which uses extends keyword(indicates that a class is inherited from another class)
+
+import java.lang.*;
+import java.io.*; 
+import java.util.*;
+class College
+{
+	String collegeName,address;
+	void showCollegeDetails()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter college name:");
+		collegeName=sc.nextLine();
+		System.out.println("Enter college address:");
+		address=sc.nextLine();
+	}
+}
+class Department extends College
+{
+	String departmentName,hodName;
+	void showDepartmentDetails()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter department name:");
+		departmentName=sc.nextLine();
+		System.out.println("Enter hod name:");
+		hodName=sc.nextLine();	
+	}
+}
+class FacultyMember extends Department
+{
+	String facultyMemberName,facultyMemberQualification;
+	int noOfYearsWorked;
+	void showfacultyMemberDetails()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter faculty member name:");
+		collegeName=sc.nextLine();
+		System.out.println("Enter faculty member qualification:");
+		address=sc.nextLine();
+		System.out.println("Enter no of years worked:");
+		noOfYearsWorked=sc.nextInt();
+	}
+}
+public class Test
+{
+	public static void main(String[]args)
+	{
+		FacultyMemberName a=new FacultyMemberName();
+		a.showCollegeDetails();
+		a.showDepartmentDetails();
+		a.showfacultyMemberDetails();
+	}
+}
