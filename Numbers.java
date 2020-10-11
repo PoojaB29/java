75ccbfd71056ffcaa48b263c12c401d8f37e92f5
PoojaB29 <58439134+PoojaB29@users.noreply.github.com>
2020-10-11 21:12:41 +0530

    Add files via upload
    The Numbers.java file includes the implementation of the applet program and the try-catch block.

---
+++ Numbers.java
@@ -0,0 +1,32 @@
+//Created by Pooja
+//On 5 Oct 2019
+//The programs icludes applet program and try-catch block 
+
+import java.applet.*;
+import java.awt.*;
+
+public class Numbers extends Applet
+{
+	public void paint(Graphics g)
+	{
+	   try
+	   {
+		for(int i = 1; i <= 10; i ++)
+		{
+			g.drawString(String.valueOf(i), 100, 100 + (i * 15));
+			Thread.sleep(100);
+		}
+	   }
+	   catch(InterruptedException e)
+	   {
+		System.out.println(e);
+	   }
+	}
+}
+
+
+/*
+<applet code="Numbers.class" width=300 height=300>
+</applet> 
+*/
+ 
